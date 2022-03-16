17634f52d26b5025336099a3568a45adac9a4347
Susanto Mandal <susanto.mandal@comprotechnologies.com>
2022-03-16 16:34:49 +0530

    removing node_modules file

---
+++ models/dbService.js
@@ -0,0 +1,28 @@
+let todoList = require("./todoList.json");
+
+const fetchDB = ()=> {
+  return todoList; 
+}
+
+const addTask = (description)=> {
+  const data = fetchDB();
+  const newTask = {
+    "id": data[data.length - 1].id + 1,
+    description,
+    "completed": false
+  }
+  data.push(newTask);
+  return newTask;
+}
+
+const findTask = (findID)=> {
+  const data = fetchDB();
+  const index = data.findIndex(task => task.id == findID );
+  return index;
+}
+
+module.exports = {
+  fetchDB,
+  addTask,
+  findTask
+}
