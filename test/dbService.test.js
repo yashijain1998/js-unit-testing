7eaa6ef62ad87b85a55dcdd49bdc0cbb5bced298
Yashi Jain <yashi.jain@comprotechnologies.com>
2022-03-22 13:43:56 +0530

    update test cases

--- test/dbService.test.js
+++ test/dbService.test.js
@@ -1,45 +1,59 @@
 const chai = require('chai');
-// const chaiHttp = require('chai-http');
-
-// const server = require('../index');
+const mongoose = require('mongoose');
 const dbService = require('../models/dbService');
+const Task = require('../models/task');
 
 const expect = chai.expect;
-// chai.use(chaiHttp);
+let id = null;
+before((done) => {
+  mongoose.connect("mongodb://localhost/task_app",{ useNewUrlParser: true})
+  .then(() => {
+    console.log("connection is successfull");
+    done();
+  })
+  .catch((error) => console.log(error));
+});
 
 describe('Testing Database fetch methods', ()=> {
-
   describe('fetch data method', ()=> {
-    it('should return data', ()=> {
-      const data = dbService.fetchDB();
+    it('should return data', async()=> {
+      const data = await dbService.fetchDB();
       expect(data).to.be.a("array");
-      // expect(data.length).to.equal(1);
     })
   })
 
   describe('add task method', ()=> {
-    it('should return a new task', ()=> {
+    it('should return a new task', async ()=> {
       const description = 'drink 4L water daily';
-      const data = dbService.addTask(description);
+      const data = await dbService.addTask(description);
+      id = data._id;
       expect(data).to.be.a("object");
       expect(data).to.have.property('_id');
       expect(data).to.have.property('description');
       expect(data).to.have.property('completed');
       expect(data.description).to.equal(description);
-      expect(data.completed).to.be.false;
+      expect(data.completed).to.be.false;  
     })
   })
 
-  describe('find task index method', ()=> {
-    it('should return index', ()=> {
-      const index = dbService.findTask(1);
-      expect(index).that.is.a('number');
-      expect(index).to.equal(0);
+  describe('update task method', ()=> {
+    it('should update task', async()=> {
+      const task = await dbService.updateStatus(id);
+      expect(task).to.be.a("object");
+      expect(task).to.have.property('_id');
+      expect(task).to.have.property('description');
+      expect(task).to.have.property('completed');
+      expect(task.completed).to.be.true;
     })
-    it('should not return index', ()=> {
-      const index = dbService.findTask(-1);
-      // expect(index).that.is.a('number');
-      expect(index).to.equal(null);
+  })
+
+  describe('delete task method', ()=> {
+    it('should delete task', async()=> {
+      const task = await dbService.deleteTaskById(id);
+      expect(task).to.be.a("object");
+      expect(task).to.have.property('_id');
+      expect(task).to.have.property('description');
+      expect(task).to.have.property('completed');
     })
   })
 
