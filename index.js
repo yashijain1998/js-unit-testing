b2b005d244b0d46ca21c24febbc59e7cee4b7553
Susanto Mandal <susanto.mandal@comprotechnologies.com>
2022-03-24 16:27:32 +0530

    add cors

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // import all the necessary packages
 const express = require("express");
-var mongoose = require("mongoose");
-let taskRoutes = require("./routes/router");
+const mongoose = require("mongoose");
+const cors = require('cors')
+const taskRoutes = require("./routes/router");
 
 // we are using port 8000
 const port = 8000;
 
-var app = express();
+const app = express();
 
 //mongodb connection 
 mongoose.connect("mongodb://localhost/task_app",{ useNewUrlParser: true})
 .then(() => console.log("connection is successfull"))
 .catch((error) => console.log(error));
 
+app.use(cors());
 app.use(express.json());
 app.use(taskRoutes);
 
